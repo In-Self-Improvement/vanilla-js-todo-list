7afb4a8035cba558e4d91ee451da4f5406569995
Bong <borobong230@gmail.com>
2024-10-12 22:18:26 +0900

    :hammer: [Update] main.js 임시 페이지 렌더링 추가 #main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
+import { createRouter } from "./lib";
+import { HomePage, LoginPage } from "./pages";
+const router = createRouter({
+  "/": HomePage,
+  "/login": LoginPage,
+});
+
+function render() {
+  const $root = document.querySelector("#root");
+
+  const Page = router.getTarget();
+  $root.innerHTML = `
+      ${Page()}
+    `;
+}
+
 function main() {
-  // main 함수
+  router.subscribe(render);
+  render();
 }
 
 main();
