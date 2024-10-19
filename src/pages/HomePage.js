45b87bca9038159160ea77f349f486ed75ada7eb
Bong <borobong230@gmail.com>
2024-10-19 22:34:26 +0900

    :hammer: [Update] 홈, 로그인 페이지 간단한 구현 추가 #main

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,9 @@
-export const LoginPage = () => {
-  return /* HTML */ ` <div>로그인페이지입니다</div> `;
+export const HomePage = () => {
+  return /* HTML */ `
+    <div>
+      <h1>환영합니다!</h1>
+      <p>여기는 간단한 홈페이지입니다.</p>
+      <button onclick="location.href='/login'">로그인하기</button>
+    </div>
+  `;
 };
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,3 +1,16 @@
-export const HomePage = () => {
-  return /* HTML */ ` <div>홈페이지 입니다.</div> `;
+export const LoginPage = () => {
+  return /* HTML */ `
+    <div>
+      <h1>로그인 페이지</h1>
+      <form>
+        <label for="username">사용자 이름:</label>
+        <input type="text" id="username" name="username" required />
+        <br />
+        <label for="password">비밀번호:</label>
+        <input type="password" id="password" name="password" required />
+        <br />
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  `;
 };
